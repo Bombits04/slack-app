dbb0af5e1b4ced3733bc6e03b5a5859448a4cf7f
Jasverenne Ferrer <jasverennepferr@gmail.com>
2024-05-27 17:19:29 +0800

    added a signup page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import Login from "../src/pages/login/login"
+import Login from "../src/pages/login/login";
 import {
   BrowserRouter,
   Routes,
@@ -8,6 +8,7 @@ import {
 } from "react-router-dom";
 import Dashboard from './pages/Dashboard/Dashboard';
 import React, { useState } from "react";
+import Signup from "../src/pages/signup/signup";
 
 
 
@@ -17,6 +18,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
+        <Route exact path="/signup" Component={Signup}/>
         <Route exact path="/dashboard" element={<Dashboard loggedin={isLoggedIn} />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/signup/signup.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const signup = () => {
+    return <div>Signup</div>
+}
+
+
+export default signup;
